d983f742e1dd42a2b8901adc9b7035bef7e28698
LucasBussink <lucas.bussink@student.graafschapcollege.nl>
2026-03-11 13:29:20 +0100

    mail

--- mail.php
+++ mail.php
@@ -1,17 +1,19 @@
 <?php
 // Email settings
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    
-    $to = "lucasbussink16@gmail.com"; // Replace with your email
-    $subject = "New Contact Form Submission";
-    $body = "Name: $name\nEmail: $email\nMessage: $message";
-    $headers = "From: $email";
-    // Send email
-    if (mail($to, $subject, $body, $headers)) {
-      echo "Email sent successfully!";
-      } else {
-        echo "Failed to send email.";
-        }
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $to = "lucasbussink16@gmail.com"; // Replace with your email
+  $subject = "New Contact Form Submission";
+  $body = "Name: $name\nEmail: $email\nMessage: $message";
+  $headers = "From: $email";
+  // Send email
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Email sent successfully!";
+  } else {
+    echo "Failed to send email.";
+  }
+}
